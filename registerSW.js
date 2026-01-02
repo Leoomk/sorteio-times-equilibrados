1fa477d5e4060b2ffaa9e92a06b0183700f51138
Leoomk <leo.goncalves@hotmail.com>
2026-01-02 12:47:01 -0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sorteio-times-equilibrados/sw.js', { scope: '/sorteio-times-equilibrados/' })})}
